3f0ac9a429dc2ceee69739c425bdd932acfff409
João Gabriel <joaogabriel9633@gmail.com>
2025-10-06 20:12:49 -0300

    set same site to none

--- backend/src/lib/jwt.ts
+++ backend/src/lib/jwt.ts
@@ -9,7 +9,7 @@ export const generateToken = (userId: string, res: Response) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // prevent CSRF attacks
+    sameSite: "none", // prevent CSRF attacks
     secure: process.env.NODE_ENV !== "development", // only send over HTTPS in production
   });
 
